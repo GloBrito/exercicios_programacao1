d35ad60bad52b3018e235a20c35a42e264236336
Glória Brito <gloriafbrito@gmail.com>
2024-08-22 23:00:30 -0300

    insere calculo exercício 7 comparar com dez

---
+++ ComparaComDez.java
@@ -0,0 +1,18 @@
+/*Algoritmo em java para ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!  */
+
+import java.util.Scanner;
+
+public class ComparaComDez {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero > 10) {
+            System.out.println("É MAIOR QUE 10!");
+        } else {
+            System.out.println("NÃO É MAIOR QUE 10!");
+        }
+    }
+}
